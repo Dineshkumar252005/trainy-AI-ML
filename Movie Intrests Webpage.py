e326214bf6d8599359703ffad12c17ff2ec0936f
DINESHKUMAR M <119733772+Dineshkumar252005@users.noreply.github.com>
2025-07-24 12:32:33 +0530

    Add files via upload

---
+++ Movie Intrests Webpage.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import pickle
+app = Flask(__name__)
+# Load the model
+model1 = pickle.load(open('C:/Users/Student/PythonProject1/movie interests/data/model.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    val1 = float(request.form['feature1'])
+    val2 = float(request.form['feature2'])
+    val3 = float(request.form['feature3'])
+    val4 = float(request.form['feature4'])
+# Format input data for prediction
+    input_data = [[val1, val2], [val3, val4]]
+    predictions = model1.predict(input_data)
+    return render_template('result.html',prediction=predictions)
+if __name__ == '__main__':
+    app.run(debug=True)
